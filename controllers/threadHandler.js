81625f7c6bfe391e1f116136acd715c86588bbbd
Glitch (ecr-anonymous-message-board) <none>
2019-12-31 14:34:42 +0000

    🛬🏓 Checkpoint ./controllers/threadHandler.js:9298160/6676

--- controllers/threadHandler.js
+++ controllers/threadHandler.js
@@ -1,24 +1,47 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-const database_uri = process.env.DATABASE_URI;
+const DatabaseURI = process.env.DATABASE_URI;
 const assert = require("chai").assert;
 
 module.exports = () => {
   this.threadList = (req, res) => {
-    MongoClient.connect(database_uri, (err, client) => {
+    MongoClient.connect(DatabaseURI, (err, client) => {
       assert.equal(null, err);
       let col = client.db("test").collection("threads");
       col
-        .find({}, { projection: { reported: 0, delete_passwords: 0 ,"replies.delete_password": 0,"replies.reported": 0} })
+        .find(
+          {},
+          {
+            projection: {
+              reported: 0,
+              delete_passwords: 0,
+              "replies.delete_password": 0,
+              "replies.reported": 0
+            }
+          }
+        )
         .sort("bumped_on", -1)
         .limit(10)
         .toArray((err, threads) => {
           assert.equal(null, err);
-        threads.
+          threads.forEach(thread => {
+            thread.replycount = thread.replies.length;
+            if (thread.replies.length > 3)
+              thread.replies = thread.replies.slice(-3);
+          });
+          res.json(threads);
         });
     });
   };
-  this.newThreat = (req, res) => {};
+  this.newThreat = (req, res) => {
+    let text = req.body.text;
+    
+    MongoClient.connect(DatabaseURI, (err, client) => {
+      assert.equal(null, err);
+      let col = client.db("test").collection("threads");
+      col.insertOne();
+    });
+  };
   this.reportThreat = (req, res) => {};
   this.deleteThreat = (req, res) => {};
 };
