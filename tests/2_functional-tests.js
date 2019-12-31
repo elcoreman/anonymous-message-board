715b5a3af9ecd6a54cb64cb369c86533628629a0
Glitch (ecr-anonymous-message-board) <none>
2019-12-31 16:30:48 +0000

    🕰💧 Checkpoint ./tests/2_functional-tests.js:9298160/11821

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,9 +7,43 @@ chai.use(chaiHttp);
 
 suite("Functional Tests", function() {
   suite("API ROUTING FOR /api/threads/:board", function() {
-    suite("POST", function() {});
+    suite("POST", function() {
+      test("create new thread", done => {
+        chai
+          .request(server)
+          .post("/api/threads/myTest")
+          .send({ text: "myText", delete_password: "pass" })
+          .end(function(err, res) {
+            assert.equal(err, null);
+            assert.equal(res.status, 200);
+          });
+      });
+    });
 
-    suite("GET", function() {});
+    suite("GET", function() {
+      test("most recent 10 threads with most recent 3 replies each", (done)=> {
+        chai
+          .request(server)
+          .get("/api/threads/myTest")
+          .end(function(err, res) {
+          assert.equal(err, null);
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.property(res.body[0], "_id");
+            assert.property(res.body[0], "created_on");
+            assert.property(res.body[0], "bumped_on");
+            assert.property(res.body[0], "text");
+            assert.property(res.body[0], "replies");
+            assert.notProperty(res.body[0], "reported");
+            assert.notProperty(res.body[0], "delete_password");
+            assert.isArray(res.body[0].replies);
+            assert.isBelow(res.body[0].replies.length, 4);
+            testId = res.body[0]._id;
+            testId2 = res.body[1]._id;
+            done();
+          });
+      });
+    });
 
     suite("DELETE", function() {});
 
