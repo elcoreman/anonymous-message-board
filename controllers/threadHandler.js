15fd7fde73f1e0074b06b038a62c937094643d08
Glitch (ecr-anonymous-message-board) <none>
2019-12-31 13:35:43 +0000

    🚤🐴 Checkpoint ./controllers/threadHandler.js:9298160/2782

--- controllers/threadHandler.js
+++ controllers/threadHandler.js
@@ -4,11 +4,18 @@ const database_uri = process.env.DATABASE_URI;
 const assert = require("chai").assert;
 
 module.exports = () => {
-  this.threatList = (req, res) => {
+  this.threadList = (req, res) => {
     MongoClient.connect(database_uri, (err, client) => {
       assert.equal(null, err);
-      let col = client.db("test").collection("threats");
-      col.find();
+      let col = client.db("test").collection("threads");
+      col
+        .find()
+      .
+        .sort("bumped_on", -1)
+        .limit(10)
+        .toArray((err, threads) => {
+          assert.equal(null, err);
+        });
     });
   };
   this.newThreat = (req, res) => {};
